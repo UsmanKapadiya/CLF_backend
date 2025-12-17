3db8ceefc0080541ab91ae445aaf6db41d52a280
Usman Kapadiya <hirenk@intellidt.com>
2025-12-17 16:02:14 +0530

    name store in about apis

--- models/About.js
+++ models/About.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const aboutSchema = new mongoose.Schema({
-
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
   title: {
     type: String,
     required: true,
